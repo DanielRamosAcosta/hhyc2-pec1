6771e40c7321f259d4b1b7773858e93efa6ea683
Daniel Ramos <danielramosacosta1@gmail.com>
2025-10-17 22:51:44 +0200

    feat: add semantic HTML structure with meta tags

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import ScrollReveal from "scrollreveal";
+
+// Initialize ScrollReveal (will be configured in later phases)
+window.sr = ScrollReveal({
+  distance: "60px",
+  duration: 800,
+  easing: "ease-in-out",
+  reset: false,
+});
