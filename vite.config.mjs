e4d0993a61aedd3e0f107f9ee0a10a90283ae6ca
Daniel Ramos <danielramosacosta1@gmail.com>
2025-10-18 21:51:37 +0200

    fix: improve accessibility by adding aria-hidden attributes to icons and updating alt text for images

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,11 +3,15 @@ import htmlMinifier from "vite-plugin-html-minifier";
 import { ViteImageOptimizer as viteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default {
-  plugins: [posthtml(), htmlMinifier({ minify: true }), viteImageOptimizer({
-    jpg: { quality: 80 },
-    png: { quality: 80 },
-    svg: { multipass: true },
-    webp: { lossless: false },
-    avif: { lossless: false },
-  })],
+  plugins: [
+    posthtml(),
+    htmlMinifier({ minify: true }),
+    viteImageOptimizer({
+      jpg: { quality: 80 },
+      png: { quality: 80 },
+      svg: { multipass: true },
+      webp: { lossless: false },
+      avif: { lossless: false },
+    }),
+  ],
 };
